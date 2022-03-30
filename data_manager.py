e50d40708374a33919abb69cceb98c4be5867556
Alex Michels <99239781+AlexMichels@users.noreply.github.com>
2022-03-30 15:23:21 +0700

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,9 @@
 import requests
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
+    # Add your API Key here
     def __init__(self):
-        self.endpoint = "https://api.sheety.co/e61a72d0959b83f64a35b5334998a592/flightDeals/prices"
+        self.endpoint = "https://api.sheety.co/[API-KEY]/flightDeals/prices"
         self.header = {"Authorization": "Bearer blablablathisisablatooken"}
         self.table = self.get_table()
         self.subscribers = self.get_subs()
@@ -19,13 +20,13 @@ class DataManager:
         return self.response.json()["prices"]
 
     def get_subs(self):
-        response = requests.get(url="https://api.sheety.co/e61a72d0959b83f64a35b5334998a592/flightDeals/users",
+        response = requests.get(url="https://api.sheety.co/[API-KEY]/flightDeals/users",
                                 headers=self.header)
         response.raise_for_status()
         return response.json()["users"]
    # changes the existing table with the put request
     def put_table(self, iata:str, row:int):
-        self.put_request = requests.put(url=f"https://api.sheety.co/e61a72d0959b83f64a35b5334998a592/flightDeals/prices/{str(row)}", json={"price": {"iataCode": iata}}, headers=self.header)
+        self.put_request = requests.put(url=f"https://api.sheety.co/[API-KEY]/flightDeals/prices/{str(row)}", json={"price": {"iataCode": iata}}, headers=self.header)
         self.put_request.raise_for_status()
         print(self.put_request)
 
@@ -33,7 +34,7 @@ class DataManager:
 
    # insert the new price
     def put_new_price(self, price:str, row:int):
-        self.put_request = requests.put(url=f"https://api.sheety.co/e61a72d0959b83f64a35b5334998a592/flightDeals/prices/{str(row)}", json={"price": {"price": price}}, headers=self.header)
+        self.put_request = requests.put(url=f"https://api.sheety.co/[API-KEY]/flightDeals/prices/{str(row)}", json={"price": {"price": price}}, headers=self.header)
         self.put_request.raise_for_status()
         print(self.put_request)
 
@@ -44,7 +45,7 @@ class DataManager:
             "lastName": last_name,
             "email": email
         }}
-        response = requests.post(url="https://api.sheety.co/e61a72d0959b83f64a35b5334998a592/flightDeals/users",
+        response = requests.post(url="https://api.sheety.co/[API-KEY]/flightDeals/users",
                                  json=body, headers=self.header)
         response.raise_for_status()
         print(response)
